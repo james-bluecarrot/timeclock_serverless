d44bf16a3b0b168e45dbc0ea26d372541f702b98
Muhammad Kamran <smkamranqadri@yahoo.com>
2016-10-11 16:57:11 +0500

    setup firebase

--- handler.js
+++ handler.js
@@ -1,5 +1,35 @@
 "use strict";
-function hello(event, context, cb) {
-    cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event: event });
+var firebase = require('firebase');
+var credentials = {
+    apiKey: "AIzaSyDI59kJ3nvFKbvvoctZuyk3IwoYuulzfwQ",
+    authDomain: "timeclock-app.firebaseapp.com",
+    databaseURL: "https://timeclock-app.firebaseio.com",
+};
+function randomString(len, charSet) {
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+        var randomPoz = Math.floor(Math.random() * charSet.length);
+        randomString += charSet.substring(randomPoz, randomPoz + 1);
+    }
+    return randomString;
 }
-exports.hello = hello;
+function initFirebase() {
+    try {
+        firebase.initializeApp(credentials);
+    }
+    catch (err) {
+        var randomNameGenerator = randomString(9);
+        firebase.initializeApp(credentials, randomNameGenerator);
+    }
+}
+function sendreport(event, context, cb) {
+    initFirebase();
+    var db = firebase.database();
+    var ref = db.ref('logging');
+    ref.once('value', function (snapshot) {
+        console.log(snapshot.val());
+        cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event: event, test: snapshot.val() });
+    });
+}
+exports.sendreport = sendreport;
--- handler.ts
+++ handler.ts
@@ -1,3 +1,38 @@
-export function hello(event, context, cb) {
-    cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+import * as path from 'path';
+import * as firebase from 'firebase';
+
+let credentials = {
+    apiKey: "AIzaSyDI59kJ3nvFKbvvoctZuyk3IwoYuulzfwQ",
+    authDomain: "timeclock-app.firebaseapp.com",
+    databaseURL: "https://timeclock-app.firebaseio.com",
+    // serviceAccount: path.resolve(__dirname, './config/TimeClock App-fb5eb2d6e36b.json')
+};
+
+function randomString(len, charSet?) {
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+    	var randomPoz = Math.floor(Math.random() * charSet.length);
+    	randomString += charSet.substring(randomPoz,randomPoz+1);
+    }
+    return randomString;
+}
+
+function initFirebase() {
+    try {
+        firebase.initializeApp(credentials);
+    } catch(err) {
+        let randomNameGenerator = randomString(9);
+        firebase.initializeApp(credentials, randomNameGenerator);
+    }
+}
+
+export function sendreport(event, context, cb) {
+    initFirebase();
+    let db = firebase.database();
+    let ref = db.ref('logging');
+    ref.once('value', (snapshot) => {
+        console.log(snapshot.val());
+        cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event, test: snapshot.val() });
+    });
 }
